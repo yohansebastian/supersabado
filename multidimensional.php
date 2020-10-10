c66f73f68ae2e70c8cee8d43ba8d5b8064e024e1
Yohan <gilsebastian505@gmail.com>
2020-10-10 12:03:16 -0500

    ejemplo array terminado

--- multidimensional.php
+++ multidimensional.php
@@ -7,12 +7,33 @@ $marca=array("Diana","Zenu","Incauca");
 
 // Arreglos en multiples dimensiones
 $productos=array(
-    "Producto1"=>array("Arroz",1600,"Diana"),
-    "Producto2"=>array("Salchicha",5000,"Zenu"),
-    "Producto3"=>array("Azucar",2500,"Incauca")
+    "Producto1"=>array("nombre"=>"Arroz","precio"=>1600,"marca"=>"Diana"),
+    "Producto2"=>array("nombre"=>"Salchicha","precio"=>5000,"marca"=>"Zenu"),
+    "Producto3"=>array("nombre"=>"Azucar","precio"=>2500,"marca"=>"Incauca")
 );
 
+//nombre,precio,marca
 print_r($productos);
+
+echo("<br>");
+echo("<br>");
+
+
+foreach($productos as $producto){
+
+    print_r($producto);
+    echo("<br>");
+
+}
+echo("<br>");
+echo("<br>");
+
+foreach($productos as $producto){
+
+    echo($producto["marca"]);
+    echo("<br>");
+
+}
 
 
 
